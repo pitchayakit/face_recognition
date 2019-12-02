1e7cf9b02433cd2d6caabf204f752682acb2a725
Pitchayakit NCU server <pitchayakit.p@gmail.com>
2019-12-02 07:53:01 +0000

    Merge with develop evn

--- web_service.py
+++ web_service.py
@@ -17,6 +17,7 @@
 # $ pip3 install flask
 
 import face_recognition
+from PIL import Image, ImageDraw
 import numpy as np
 from flask import Flask, jsonify, request, redirect
 from face_encoding import *
@@ -93,6 +94,12 @@ def detect_faces_in_image(file_stream):
 
     if len(face_locations) > 0:
         face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+        
+        # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
+        # See http://pillow.readthedocs.io/ for more about PIL/Pillow
+        pil_image = Image.fromarray(unknown_image)
+        # Create a Pillow ImageDraw Draw instance to draw with
+        draw = ImageDraw.Draw(pil_image)
 
         # Loop through each face found in the unknown image
         for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
